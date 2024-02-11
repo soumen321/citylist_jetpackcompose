d91f5df8c5157996d413386eaa60015fe16ec356
soumen321 <bhattacharjee.soumen@gmail.com>
2024-02-11 13:24:28 +0530

    feature:ceate data class

---
+++ app/src/main/java/com/example/citylistjetpack/data/model/CityList.kt
@@ -0,0 +1,21 @@
+package com.example.citylistjetpack.data.model
+
+/*
+* "city": "Melbourne",
+    "lat": "-37.8142",
+    "lng": "144.9631",
+    "country": "Australia",
+    "iso2": "AU",
+    "admin_name": "Victoria",
+    "capital": "admin",
+    "population": "4529500",
+    "population_proper": "4529500"
+    * */
+
+data class CityList(
+    val city : String,
+    val capital : String,
+    val population : String,
+    val lat : String,
+    val lng : String
+)
